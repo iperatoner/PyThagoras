e83f622c1ff8273ff8edf93efa62db06c68573a4
Patrick Geltinger <patlkli@athene.(none)>
2010-04-25 21:14:14 +0200

    Added pi() calculation function

--- exactmath.py
+++ exactmath.py
@@ -60,6 +60,25 @@ PI = Decimal(str('''3.1415926535897932384626433832
 
 getcontext().prec = PREC
 
+def pi():
+    """Compute Pi to the current precision.
+
+    >>> print pi()
+    3.141592653589793238462643383
+
+    """
+    getcontext().prec += 2  # extra digits for intermediate steps
+    three = Decimal(3)      # substitute "three=3.0" for regular floats
+    lasts, t, s, n, na, d, da = 0, three, 3, 1, 0, 0, 24
+    while s != lasts:
+        lasts = s
+        n, na = n+na, na+8
+        d, da = d+da, da+32
+        t = (t * n) / d
+        s += t
+    getcontext().prec -= 2
+    return +s               # unary plus applies the new precision
+
 def sin(x):
 
     getcontext().prec += 2
